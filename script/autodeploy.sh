827c12ff178a10e6692822e40cf2acc4a5174c9d
New Panuwat <new_sbh@me.com>
2022-07-22 10:51:31 +0700

    change auto deploy sctipty

--- script/autodeploy.sh
+++ script/autodeploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ssh root@45.32.100.111 <<EOF
- cd ~/next-container && git pull --rebase && docker-compose up -d
+ cd ~/next-docker-compose && git pull --rebase && docker-compose up -d
  exit
 EOF
